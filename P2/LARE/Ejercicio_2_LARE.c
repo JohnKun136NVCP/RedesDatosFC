c3ab8b1e086a0bea995f154921efdccc8b154f39
RaulLabonne <raul_labonne27@ciencias.unam.mx>
2025-08-24 00:26:50 -0600

    docs: Documentacion de ejercicio 2

--- P2/LARE/Ejercicio_2_LARE.c
+++ P2/LARE/Ejercicio_2_LARE.c
@@ -1,12 +1,14 @@
 #include "Ejercicio_1_LARE.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+// Nuestro Buffer para leer
 #define MAX_LEN 100
 
 int main(int argc, char *argv[]) {
   if (argc != 3) {
-    printf("Type: <archive> <number> \n The number must be between 1 and 26");
+    printf("Type: %s <archive> <number> \n The number must be between 1 "
+           "and 26",
+           argv[0]);
     return 1;
   }
 
@@ -15,19 +17,25 @@ int main(int argc, char *argv[]) {
   int shift = atoi(argv[2]) % 26;
   char line[MAX_LEN];
 
+  // Generamos un archivo temporal donde guardaremos el contenido de la entrada
+  // ya encriptado
   stream = fopen(argv[1], "r");
-  exit = fopen("temp.txt", "w");
+  exit = fopen("temp.txt", "w"); // El archivo temporal
 
   while (fgets(line, MAX_LEN, stream) != NULL) {
-    encryptCaesar(line, shift);
-    fprintf(exit, "%s", line);
+    encryptCaesar(line, shift); // Encriptamos...
+    fprintf(exit, "%s",
+            line); // Una vez encriptado, escribimos en nuestra salida tempora;
   }
 
+  // Terminando de leer y escribir, cerramos el flujo en ambos archivos de texto
   fclose(stream);
   fclose(exit);
 
+  // Borramos el archivo original ya que no esta encriptado y renombramos al
+  // temporal como el original
   remove(argv[1]);
-  rename("temp.txt", argv[1]);
+  rename("temp.txt", argv[1]); // El archivo ya esta encriptado
 
   return 0;
 }
