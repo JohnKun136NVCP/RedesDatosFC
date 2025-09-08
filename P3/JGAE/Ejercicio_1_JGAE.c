6b65b022e6d21a69988b797039461a9758797136
Aldo Jurado <emilio.jurado@ciencias.unam.mx>
2025-09-07 20:02:11 -0600

    ej1->servidorV1, ej2->clienteV1, ej3->servidorV2,ej4->clienteV2

--- P3/JGAE/Ejercicio_1_JGAE.c
+++ P3/JGAE/Ejercicio_1_JGAE.c
@@ -8,9 +8,6 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-// Biblioteca para trabajar con hilos
-#include <pthread.h>
-
 #define BUFFER_SIZE 1024 // Tama~no del buffer para recibir datos
 
 /**
@@ -67,6 +64,66 @@ void sendFile(const char *filename, int sockfd)
     fclose(fp);
 }
 
+// Función para convertir a minúsculas
+void toLowerCase(char *str)
+{
+    for (int i = 0; str[i]; i++)
+        str[i] = tolower((unsigned char)str[i]);
+}
+
+// Función para eliminar espacios al inicio y final
+void trim(char *str)
+{
+    char *end;
+    while (isspace((unsigned char)*str))
+        str++; // inicio
+    end = str + strlen(str) - 1;
+    while (end > str && isspace((unsigned char)*end))
+        end--; // final
+    *(end + 1) = '\0';
+}
+
+bool isOnFile(const char *bufferOriginal)
+{
+    FILE *fp;
+    char line[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
+    bool foundWorld = false;
+    // Copiamos el buffer original para poder modificarlo
+    strncpy(buffer, bufferOriginal, BUFFER_SIZE);
+    buffer[BUFFER_SIZE - 1] = '\0'; // aseguramos terminación
+    trim(buffer);
+    toLowerCase(buffer);
+    fp = fopen("cipherworlds.txt", "r");
+    if (fp == NULL)
+    {
+        printf("[-]Error opening file!\n");
+        return false;
+    }
+    while (fgets(line, sizeof(line), fp) != NULL)
+    {
+        // Obtener el tamaño de la cadena hasta antes del salto de línea
+        // Luego con ese número sabe dónde está el salto de línea y reemplaza el salto de línea por
+        // caracter del final de una cadena
+        line[strcspn(line, "\n")] = '\0';
+        // Elimina espacios en blanco al inicio y al final
+        trim(line);
+        // Convierte a minúsculas
+        toLowerCase(line);
+        // Compara la línea del buffer con la línea que está leyendo del archivo
+        if (strcmp(line, buffer) == 0)
+        {
+            // Si encuentra la palabra anota que la encontró
+            foundWorld = true;
+            break;
+        }
+    }
+    // cierra el archivo
+    fclose(fp);
+    // Devuelve si la encontró o no
+    return foundWorld;
+}
+
 /*
 Imprime el contenido de un archivo  línea a línea
 */
@@ -90,192 +147,139 @@ void imprimirArchivo(const char *filename)
     fclose(fp);
 }
 
-/*
- * Función para crear un socket IPv4 TCP
- */
-int crearSocketIPv4TCP()
+int main(int argc, char *argv[])
 {
-    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    int server_sock, client_sock;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_size;
+    char buffer[BUFFER_SIZE] = {0};
+    int puertoClient;
+    int shift;
+    int puertoServer;
+    char fileName[BUFFER_SIZE / 2];
+    char fileComplete[BUFFER_SIZE];
+
+    if (argc != 2)
+    {
+        printf("Type: %s <port> \n", argv[0]);
+        return 1;
+    }
+
+    puertoServer = atoi(argv[1]);
+
+    // Crear el socket:
+    // AF_INET indica que se utilizará IPv4
+    // SOCK_STREAM indica que se utilizará TCP
+    // 0 indica que se utilizará el protocolo predeterminado (TCP en este caso)
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
     if (server_sock == -1)
     {
         perror("[-] Error to create the socket");
-        return -1;
+        return 1;
     }
-    return server_sock;
-}
 
-/*
-Función para configurar el socket servidor, debe recibir un puntero a la estrcutura para que sí se modifique y no sea paso por valor
-*/
-int configurarEnlazarSocket(struct sockaddr_in *server_addr, int puertoServer, int server_sock)
-{
-    // Configuramos el socket
-    server_addr->sin_family = AF_INET;
-    server_addr->sin_port = htons(puertoServer);
-    server_addr->sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(puertoServer);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    // Enlazamos el socket y hacemos que escuche en el puerto deseado
-    if (bind(server_sock, (struct sockaddr *)server_addr, sizeof(*server_addr)) < 0)
+    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
         perror("[-] Error binding");
         close(server_sock);
-        return -1;
+        return 1;
     }
-    return 0;
-}
 
-/*
- * Función para escuchar y aceptar conexiones de clientes
- Es importante que client_sock y client_addr sean paso por referencia porque queremos que en el código original sí se modifiquen con
- el entero representación del socket cliente y su dirección respectivamente
- */
-int escucharAceptarServidor(int server_sock, int puertoServer, int *client_sock, struct sockaddr_in *client_addr)
-{
-    // Estructura para el tamaño de la dirección
-    socklen_t addr_size;
-
-    // Ponemos al socket a escuchar en el puerto deseado
     if (listen(server_sock, 1) < 0)
     {
         perror("[-] Error on listen");
         close(server_sock);
-        return -1;
-    }
-    printf("[+] Server listening port %d...\n", puertoServer);
-
-    // Bloqueamos esperando a que un cliente se conecte
-    addr_size = sizeof(*client_addr);
-    *client_sock = accept(server_sock, (struct sockaddr *)client_addr, &addr_size);
-    if (*client_sock < 0)
-    {
-        perror("[-] Error on accept");
-        close(server_sock);
-        return -1;
-    }
-    // printf("[+] Client connected\n"); Comento lo de client connected ahorita que ya tenemos 3 servidores
-}
-
-int manejadorCliente(int client_sock, int server_sock, int puertoServer)
-{
-    // Buffer para transferencia de datos
-    char buffer[BUFFER_SIZE] = {0};
-    // Puerto al que quiere conectarse el cliente
-    int puertoClient;
-    // Desplazamiento del cliente para cifrar
-    int shift;
-    // Nombre del archivo que se recibirá
-    char fileName[BUFFER_SIZE / 2];
-    // Nombre del archivo que se recibirá + la leyenda encrypted_
-    char fileComplete[BUFFER_SIZE];
-
-    // recibimos nombre del archivo y desplazamiento
-    int bytes = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
-    if (bytes <= 0)
-    {
-        printf("[-] Missed file name or shift\n");
-        close(client_sock);
-        close(server_sock);
-        return -1;
-    }
-    buffer[bytes] = '\0';
-    sscanf(buffer, "%s %d", fileName, &shift); // extrae nombre del archivo y desplazamiento
-
-    // Avisamos que estamos listos para procesar el archivo
-    // Si el puerto es válido avisamos que está autorizado
-    // printf("[+][Server %d] Request accepted... %d\n", puertoServer, puertoClient);
-    send(client_sock, "REQUEST ACCEPTED", strlen("REQUEST ACCEPTED"), 0);
-
-    // Formateamos el nombre del archivo agregando la leyenda encrypted_
-    snprintf(fileComplete, BUFFER_SIZE, "encrypted_%s", fileName);
-    // Eliminamos saltos de línea y retornos de carro del nombre del archivo, reemplazándolos por el caracter nulo
-    fileName[strcspn(fileName, "\r\n")] = '\0';
-
-    // Abrimos el archivo que vamos a guardar encriptado
-    FILE *fp = fopen(fileComplete, "w");
-    if (fp == NULL)
-    {
-        perror("[-] Error to open the file");
-        close(client_sock);
-        return -1;
-    }
-
-    // Recibimos el archivo en bloques por medio del buffer
-    while ((bytes = recv(client_sock, buffer, sizeof(buffer), 0)) > 0)
-    {
-        // cada bloque se cifra y se escribe en el archivo
-        caesar(buffer, bytes, shift, 1);
-        fwrite(buffer, 1, bytes, fp);
-    }
-
-    // Avisamos que el archivo fue cifrado
-    send(client_sock, "FILE RECEIVED AND ENCRYPTED", strlen("FILE RECEIVED AND ENCRYPTED"), 0);
-    printf("[+][Server %d] File received and encrypted:\n", puertoServer);
-
-    fclose(fp);
-    // Imprimir el archivo cifrado
-    imprimirArchivo(fileComplete);
-
-    close(client_sock);
-    close(server_sock);
-}
-
-/**
- * Función que contiene todo el programa del servidor.
- */
-void *
-programaServidor(void *arg)
-{
-    // Casteamos los argumentos del tipo genérico al array de argumentos
-    char *puertoChar = (char *)arg;
-    int puertoServer = atoi(puertoChar);
-
-    // enteros representación de los sockets
-    int server_sock, client_sock;
-    // Estructura definición de la dirección de los sockets
-    struct sockaddr_in server_addr, client_addr;
-
-    // Crear el socket IPv4 TCP:
-    if ((server_sock = crearSocketIPv4TCP()) < 0)
-        return NULL;
-
-    // Configuramos el socket pasando la dirección de memoria donde guardamos la configuración
-    if (configurarEnlazarSocket(&server_addr, puertoServer, server_sock) < 0)
-        return NULL;
-
-    // POnemos al socket a escuchar en el puerto deseado
-    if (escucharAceptarServidor(server_sock, puertoServer, &client_sock, &client_addr) < 0)
-        return NULL;
-
-    // Manejador del cliente
-    if (manejadorCliente(client_sock, server_sock, puertoServer) < 0)
-        return NULL;
-}
-
-int main(int argc, char *argv[])
-{
-
-    if (argc != 4)
-    {
-        printf("Type: %s <port1> <port2> <port3> \n", argv[0]);
         return 1;
     }
 
-    // Funcionalidad para el manejo de 3 clientes
-    pthread_t t1, t2, t3;
+    printf("[+] Server listening port %d...\n", puertoServer);
 
-    // El argumento del programa es un puerto para el servidor
-    char *puerto1 = argv[1];
-    char *puerto2 = argv[2];
-    char *puerto3 = argv[3];
+    addr_size = sizeof(client_addr);
+    client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &addr_size);
+    if (client_sock < 0)
+    {
+        perror("[-] Error on accept");
+        close(server_sock);
+        return 1;
+    }
+    printf("[+] Client connected\n");
 
-    // Creamos los hilos con la función del programa y el puerto correspondiente
-    pthread_create(&t1, NULL, programaServidor, puerto1);
-    pthread_create(&t2, NULL, programaServidor, puerto2);
-    pthread_create(&t3, NULL, programaServidor, puerto3);
+    int bytes = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
+    if (bytes <= 0)
+    {
+        printf("[-] Missed port or shift\n");
+        close(client_sock);
+        close(server_sock);
+        return 1;
+    }
+    buffer[bytes] = '\0';
 
-    // Hacemos que el hilo main espere a que terminen los demás hilos
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
-    pthread_join(t3, NULL);
+    sscanf(buffer, "%d %d", &puertoClient, &shift); // extrae puerto y desplazamiento
+
+    // Validamos que el puerto recibido sea igual al puerto de mi socket
+    if (puertoClient == puertoServer)
+    {
+        // Si el puerto es válido avisamos que está autorizado
+        printf("[+][Server %d] Request accepted... %d\n", puertoServer, puertoClient);
+        send(client_sock, "REQUEST ACCEPTED", strlen("REQUEST ACCEPTED"), 0);
+
+        // recibimos el nombre del archivo
+        bytes = recv(client_sock, fileName, sizeof(fileName) - 1, 0);
+        if (bytes <= 0)
+        {
+            printf("[-] Error receiving file name\n");
+            close(client_sock);
+            close(server_sock);
+            return 1;
+        }
+        // caracter nulo para que el final del nombre del archivo sea correcto
+        fileName[bytes] = '\0';
+
+        // Enviamos confirmación de recibido del nombre del archivo
+        send(client_sock, "FILENAME RECEIVED", strlen("FILENAME RECEIVED"), 0);
+
+        // Formateamos el nombre del archivo agregando la leyenda encrypted_
+        snprintf(fileComplete, BUFFER_SIZE, "encrypted_%s", fileName);
+        // Eliminamos saltos de línea y retornos de carro del nombre del archivo, reemplazándolos por el caracter nulo
+        fileName[strcspn(fileName, "\r\n")] = '\0';
+
+        // Abrimos el archivo que vamos a guardar encriptado
+        FILE *fp = fopen(fileComplete, "w");
+        if (fp == NULL)
+        {
+            perror("[-] Error to open the file");
+            close(client_sock);
+            return 1;
+        }
+
+        // Recibimos el archivo en bloques por medio del buffer
+        while ((bytes = recv(client_sock, buffer, sizeof(buffer), 0)) > 0)
+        {
+            // cada bloque se cifra y se escribe en el archivo
+            caesar(buffer, bytes, shift, 1);
+            fwrite(buffer, 1, bytes, fp);
+        }
+
+        // Avisamos que el archivo fue cifrado
+        send(client_sock, "FILE RECEIVED AND ENCRYPTED", strlen("FILE RECEIVED AND ENCRYPTED"), 0);
+        printf("[+][Server %d] File received and encrypted:\n", puertoServer);
+
+        fclose(fp);
+        // Imprimir el archivo cifrado
+        imprimirArchivo(fileComplete);
+    }
+    else
+    {
+        // si el puerto no es entonces mandamos REJECTED
+        send(client_sock, "REJECTED", strlen("REJECTED"), 0);
+        printf("[+][Server %d] Request rejected (Client requested port %d)\n", puertoServer, puertoClient);
+    }
+
+    close(client_sock);
+    close(server_sock);
     return 0;
 }
--- P3/JGAE/Ejercicio_2_JGAE.c
+++ P3/JGAE/Ejercicio_2_JGAE.c
@@ -5,20 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// Biblioteca para trabajar con hilos
-#include <pthread.h>
 
 #define BUFFER_SIZE 1024
-
-struct datos_programa
-{
-    int *client_sock;
-    struct sockaddr_in *serv_addr;
-    const char *server_ip;
-    int puerto;
-    char *rutaArchivo;
-    char *shift;
-};
 
 void sendFile(const char *filename, int sockfd)
 {
@@ -41,7 +29,7 @@ void sendFile(const char *filename, int sockfd)
     fclose(fp);
 }
 
-/* Función para configuración del socket y conexión al servidor
+/* Función para configuración se socket y conexión al servidor
  */
 int conectarServidor(struct sockaddr_in *serv_addr, int client_sock, const char *server_ip, int puerto)
 {
@@ -59,15 +47,13 @@ int conectarServidor(struct sockaddr_in *serv_addr, int client_sock, const char 
     return 0;
 }
 
-/* Función para autorizar el envío del archivo al servidor y enviarlo si es autorizado respecto a la ruta y el desplazamiento
- */
-void autorizarEnviarArchivo(int client_sock, int puerto, char *shift, char *rutaArchivo)
+void autorizarEnviarArchivo(int client_sock, char *puerto, char *shift, char *rutaArchivo, int puertoServer)
 {
     char mensaje[BUFFER_SIZE];
     char buffer[BUFFER_SIZE] = {0};
 
     // Formateamos y enviamos puerto objetivo y desplazamiento
-    snprintf(mensaje, sizeof(mensaje), "%s %s", rutaArchivo, shift);
+    snprintf(mensaje, sizeof(mensaje), "%s %s", puerto, shift);
     send(client_sock, mensaje, strlen(mensaje), 0);
 
     // Esperamos la respuesta del servidor con recv que recibe datos de un socket ya conectado
@@ -79,24 +65,33 @@ void autorizarEnviarArchivo(int client_sock, int puerto, char *shift, char *ruta
         if (strstr(buffer, "REQUEST ACCEPTED") != NULL)
         {
 
-            // enviamos el archivo
-            sendFile(rutaArchivo, client_sock);
+            // Enviamos el nombre del archivo
+            send(client_sock, rutaArchivo, strlen(rutaArchivo), 0);
 
-            // indicamos que ya no enviaremos más datos
-            shutdown(client_sock, SHUT_WR);
-
-            // Esperamos confiramación de recibido y cifrado
+            // Esperamos confirmación de recibido
             bytes = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
-            if (bytes > 0)
+            if (bytes > 0 && (strstr(buffer, "FILENAME RECEIVED") != NULL))
             {
-                buffer[bytes] = '\0';
-                printf("[*] SERVER RESPONSE %d: %s\n", puerto, buffer);
+
+                // enviamos el archivo
+                sendFile(rutaArchivo, client_sock);
+
+                // indicamos que ya no enviaremos más datos
+                shutdown(client_sock, SHUT_WR);
+
+                // Esperamos confiramación de recibido y cifrado
+                bytes = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
+                if (bytes > 0)
+                {
+                    buffer[bytes] = '\0';
+                    printf("[*] SERVER RESPONSE %d: %s\n", puertoServer, buffer);
+                }
+                close(client_sock);
             }
-            close(client_sock);
         }
         else
         {
-            printf("[*] SERVER RESPONSE %d: %s\n", puerto, buffer);
+            printf("[*] SERVER RESPONSE %d: %s\n", puertoServer, buffer);
             close(client_sock);
         }
     }
@@ -106,94 +101,40 @@ void autorizarEnviarArchivo(int client_sock, int puerto, char *shift, char *ruta
     }
 }
 
-/*
-Función principal que ejecutará cada hilo del cliente para conectarse a un servidor con un puerto específico y enviar un archivo.
-*/
-void *programaCliente(void *arg)
-{
-    struct datos_programa *datos = (struct datos_programa *)arg;
-    int *client_sock = datos->client_sock;
-    struct sockaddr_in *serv_addr = datos->serv_addr;
-    const char *server_ip = datos->server_ip;
-    int puerto = datos->puerto;
-    char *rutaArchivo = datos->rutaArchivo;
-    char *shift = datos->shift;
-
-    // Creamos el socket de tipo TCP ipv4
-    *client_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (*client_sock == -1)
-    {
-        perror("[-] Error to create the socket");
-        return NULL;
-    }
-    if (conectarServidor(serv_addr, *client_sock, server_ip, puerto) != 1)
-    {
-        autorizarEnviarArchivo(*client_sock, puerto, shift, rutaArchivo);
-    }
-    close(*client_sock);
-}
-
 int main(int argc, char *argv[])
 {
-    if (argc != 9)
+    if (argc != 5)
     {
-        printf("Type: %s <server_ip> <port1> <port2> <port3> <file_path1> <file_path2> <file_path3> <shift>\n", argv[0]);
+        printf("Type: %s <server_ip> <port> <shift> <file_path>\n", argv[0]);
         return 1;
     }
 
-    // Obtenemos los argumentos que van a necesitar todos los hilos
     char *server_ip = argv[1];
-    int puerto1 = atoi(argv[2]);
-    int puerto2 = atoi(argv[3]);
-    int puerto3 = atoi(argv[4]);
-    char *rutaArchivo1 = argv[5];
-    char *rutaArchivo2 = argv[6];
-    char *rutaArchivo3 = argv[7];
-    char *shift = argv[8];
-    int client_sock1;
-    struct sockaddr_in serv_addr1;
-    int client_sock2;
-    struct sockaddr_in serv_addr2;
-    int client_sock3;
-    struct sockaddr_in serv_addr3;
+    char *puerto = argv[2];
+    char *shift = argv[3];
+    char *rutaArchivo = argv[4];
 
-    // Creamos los 3 struct para datos del programa que pasaremos como argumento
-    struct datos_programa datos1;
-    datos1.client_sock = &client_sock1;
-    datos1.serv_addr = &serv_addr1;
-    datos1.server_ip = server_ip;
-    datos1.puerto = puerto1;
-    datos1.rutaArchivo = rutaArchivo1;
-    datos1.shift = shift;
+    int client_sock;
+    struct sockaddr_in serv_addr;
 
-    struct datos_programa datos2;
-    datos2.client_sock = &client_sock2;
-    datos2.serv_addr = &serv_addr2;
-    datos2.server_ip = server_ip;
-    datos2.puerto = puerto2;
-    datos2.rutaArchivo = rutaArchivo2;
-    datos2.shift = shift;
+    int i;
+    int puertos[3] = {49200, 49201, 49202};
 
-    struct datos_programa datos3;
-    datos3.client_sock = &client_sock3;
-    datos3.serv_addr = &serv_addr3;
-    datos3.server_ip = server_ip;
-    datos3.puerto = puerto3;
-    datos3.rutaArchivo = rutaArchivo3;
-    datos3.shift = shift;
+    for (i = 0; i < 3; i++)
+    {
+        // Creamos el socket de tipo TCP ipv4
+        client_sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (client_sock == -1)
+        {
+            perror("[-] Error to create the socket");
+            return 1;
+        }
+        if (conectarServidor(&serv_addr, client_sock, server_ip, puertos[i]) != 1)
+        {
+            autorizarEnviarArchivo(client_sock, puerto, shift, rutaArchivo, puertos[i]);
+        }
+    }
 
-    // Funcionalidad para el envío de 3 archivos a 3 puertos diferentes
-    pthread_t t1, t2, t3;
-
-    // Creamos los hilos con la función del programa y el puerto correspondiente
-    pthread_create(&t1, NULL, programaCliente, &datos1);
-    pthread_create(&t2, NULL, programaCliente, &datos2);
-    pthread_create(&t3, NULL, programaCliente, &datos3);
-
-    // Hacemos que el hilo main espere a que terminen los demás hilos
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
-    pthread_join(t3, NULL);
-
+    close(client_sock);
     return 1;
 }
